94ed0f408b13e579f8914b9a072a4d2f2524118d
PHAMVOMINHTRUNG <minhtrung9595@gmail.com>
2019-08-20 19:42:08 +0700

    update VS6 add websub signup

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import HomeView
+from . import views
 urlpatterns = [
-   
     path('', HomeView.as_view(), name ='index'),
+    path('dangky/',views.dangky,name="dang ky"),
 ]
--- core/views.py
+++ core/views.py
@@ -3,11 +3,10 @@ from django.views import View
 from django.http import HttpResponse,HttpResponseRedirect
 # Create your views here.
 
-
 class HomeView(View):
     def get(self,request):
         return render(request,'homepage/index.html')
-    
 
 def dangky(request):
     return render(request,'homepage/dangky.html')
+
