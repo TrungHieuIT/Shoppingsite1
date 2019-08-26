489d3964f3fefa9d4648362e50aa06ed892107ff
PHAMVOMINHTRUNG <minhtrung9595@gmail.com>
2019-08-26 17:56:34 +0700

    update VS 25
    update cart and js

--- static/homepage/js/main_javascript.js
+++ static/homepage/js/main_javascript.js
@@ -5,3 +5,38 @@ function showDialog(){
 }
 
 /*document.getElementById("quantity_input").value ="110";*/
+
+function ready() {
+    var removeCartItemButtons = document.getElementsByClassName('btn_remove_gioHang')
+    for (var i = 0; i < removeCartItemButtons.length; i++) {
+        var button = removeCartItemButtons[i]
+        button.addEventListener('click', removeCartItem)
+    }
+}
+
+function addItemToCart(image,title,price){
+    var cart_row = document.createElement('div')
+    cart_row.classList.add(cart_row)
+    var cart_image = document.getElementsByClassName("image_SPCT")
+    var cart_title = document.getElementsByClassName("product_name")
+    var cart_price = document.getElementsByClassName("product_price")
+    for (var i = 0; i < cart_title.length; i++) {
+        if (cart_title[i].innerText == cart_title) {
+            alert('This item is already added to the cart')
+            return
+        }
+    }
+    var cartRowContents = `
+            <div class="inner">
+            <img  class="image_SPCT" src="{% static 'media/'%}{{item.pro_image}}" alt="{{item.pro_name}}">
+            </div>
+            </div>
+            <div class="product_content">		
+                <div class="product_price discount">{{item.pro_price}}VND<span></span></div>
+                <div class="product_name"><div>{{item.pro_name}}</div></div>
+            </div>`
+    cartRow.innerHTML = cartRowContents
+    cartItems.append(cartRow)
+    cartRow.getElementsByClassName('btn-danger')[0].addEventListener('click', removeCartItem)
+    cartRow.getElementsByClassName('cart-quantity-input')[0].addEventListener('change', quantityChanged)
+}
